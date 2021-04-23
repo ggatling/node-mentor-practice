d2020731dc55d172ba44638cd29eab831f64b5f4
Gerda Gatling <gerda.gatling@gmail.com>
2021-04-23 11:55:11 -0400

    more auth

--- Authentication.js
+++ Authentication.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const router = express.Router();
 
 router.post('/login',loginUser);
 router.post('/logout', logout);
 
-passport.use(
-    'local-login-spa',
-    new LocalStrategy(
-        {
-            username: 'login',
-            password: 'password',
-        },
-        async (login, password, callback) => {
-            try
-        }
-    )
-)
+//below code is for session stoarge using cookies
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
+
+
+function loginUser(req,res,callback) {
+    const handler = passport.authenticate({})
+    handler(req,res,callback)
+}
